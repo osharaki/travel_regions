78622a770ff7b1204a2a6e4ee6277d1438255620
Omar Sharaki <omarsharaki@gmail.com>
2020-09-15 15:47:56 +0200

    :sparkles: Implemented write to CSV

--- src/csvIO.py
+++ src/csvIO.py
@@ -1,7 +1,8 @@
 import csv
 from typing import List
 
-def readData(path: str) -> List[str]:
+
+def readCSV(path: str) -> List[str]:
     with open(path, newline="", encoding="utf-8",) as csvfile:
         csvReader = csv.reader(csvfile, delimiter=",")
         data: List[str] = []
@@ -10,9 +11,25 @@ def readData(path: str) -> List[str]:
         return data
 
 
-def writeData(path: str = "output/data.csv"):
+def writeCSV(
+    data: List[List[str]],
+    headers: List[str] = [
+        "",
+        "community_1",
+        "community_2",
+        "community_3",
+        "community_4",
+        "community_5",
+        "community_6",
+        "country_code",
+        "latitude",
+        "longitude",
+        "place_name",
+    ],
+    path: str = "output/data.csv",
+):
     with open(path, "w", newline="", encoding="utf-8") as csvfile:
-        csvWriter = csv.writer(csvfile, delimiter=" ")
-        csvWriter.writerow(["Spam"] * 5 + ["Baked Beans"])
-        csvWriter.writerow(["Spam", "Lovely Spam", "Wonderful Spam"])
+        csvWriter = csv.writer(csvfile, delimiter=",")
+        csvWriter.writerow(headers)
+        csvWriter.writerows(data)
 
--- src/main.py
+++ src/main.py
@@ -4,19 +4,26 @@ from typing import Dict, List
 
 
 def main():
-    writeData()
-    data: List[str] = readData(
+    data: List[str] = readCSV(
         "data/communities_-1__with_distance_multi-level_geonames_cities_7.csv"
     )
     l1Communities: Dict[int, List[List[str]]] = getCommunities(data, 1)
     l2CommunitiesLatinAmerica: Dict[int, List[List[str]]] = getCommunities(
         l1Communities[0], 2
     )
-    for l2CommunityId in l2CommunitiesLatinAmerica:
+    writeData: List[List[str]] = [
+        memberAtId
+        for l2CommunityId in l2CommunitiesLatinAmerica
+        for memberAtId in l2CommunitiesLatinAmerica[l2CommunityId]
+    ]
+    writeCSV(
+        writeData,
+        path="C:/Users/osharaki/OneDrive - Technische Universitat Munchen/programming_misc/WebDev/Leaflet_Sandbox/data.csv",
+    )
+    """ for l2CommunityId in l2CommunitiesLatinAmerica:
         for memberAtId in l2CommunitiesLatinAmerica[l2CommunityId]:
             print(memberAtId)
-        print(f"End members in community {l2CommunityId}")
-    # TODO: Write the generated communities to a CSV file and read it in Leaflet
+        print(f"End members in community {l2CommunityId}") """
 
 
 if __name__ == "__main__":
