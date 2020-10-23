d00c7164d78a62a5f37152b81ec827f886a4d61a
Omar Sharaki <omarsharaki@gmail.com>
2020-10-23 19:17:21 +0200

    :art: Improved output file formatting

--- src/jsonIO.py
+++ src/jsonIO.py
@@ -5,7 +5,7 @@ def clusterToJSON(polygons, clusters, target):
     data['polygons'] = polygons
     data['clusters'] = clusters
     with open(target, 'w') as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=4)
     """
     {
         polygons: []
