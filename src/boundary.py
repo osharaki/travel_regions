b3aae832db0b5d91d2981f2a6bc328b7c2be54d9
Omar Sharaki <omarsharaki@gmail.com>
2020-11-02 16:04:37 +0100

    :building_construction: generateConstrainedVoronoiDiagram now receives constraint geometry as an argument

--- src/boundary.py
+++ src/boundary.py
@@ -1,10 +1,18 @@
 # http://blog.thehumangeo.com/2014/05/12/drawing-boundaries-in-python/
-from typing import Dict, List, Tuple
 import shapely.geometry as geometry
 import matplotlib.pyplot as pl
-from descartes import PolygonPatch
 import alphashape
 import time
+import os
+import numpy as np
+
+from typing import Dict, List, Tuple
+from descartes import PolygonPatch
+from geovoronoi import voronoi_regions_from_coords, coords_to_points
+from shapely.ops import transform
+
+from shpIO import *
+
 
 def generateConcaveHull(points: List[Tuple[float, float]], visualize=False):
     start = time.time()
@@ -13,15 +21,32 @@ def generateConcaveHull(points: List[Tuple[float, float]], visualize=False):
     ax.scatter(*zip(*points))
     ax.add_patch(PolygonPatch(alpha_shape, alpha=0.2))
     end = time.time()
-    print(f'Elapsed time: {end-start}')
-    if(visualize):
+    print(f"Elapsed time: {end-start}")
+    if visualize:
         pl.show()
     return list(zip(*alpha_shape.exterior.coords.xy))
+
 
 def generateConvexHull(points: List[Tuple[float, float]]):
     point_collection = geometry.MultiPoint(points)
     pp = PolygonPatch(point_collection.convex_hull)
     return pp._path.vertices.tolist()
+
+
+def generateConstrainedVoronoiDiagram(points, containingArea):
+
+    points = np.array(points)
+
+    coords = coords_to_points(points)
+    # use only the points inside the geographic area
+    containedPoints = []
+    for p in coords:  # converts to shapely Point
+        if p.within(containingArea):
+            containedPoints.append(p)
+    poly_shapes, pts, poly_to_pt_assignments = voronoi_regions_from_coords(
+        points, containingArea
+    )
+    return [poly_shapes, pts, poly_to_pt_assignments]
 
 
 def plot_polygon(polygon, figure):
@@ -33,3 +58,4 @@ def plot_polygon(polygon, figure):
     patch = PolygonPatch(polygon, fc="#999999", ec="#000000", fill=True, zorder=-1)
     ax.add_patch(patch)
     return figure
+
