56b71b554c1ad65ea8e191314b7011bb9800490d
Omar Sharaki <omarsharaki@gmail.com>
2020-11-02 16:05:03 +0100

    :sparkles: Added funtion to read geojson files

--- src/jsonIO.py
+++ src/jsonIO.py
@@ -1,12 +1,10 @@
 from typing import Dict, List, Tuple
 import json
 
-
 def clusterToJSON(data: Dict[str, List[List[float]]], target):
     with open(target, "w") as f:
         json.dump(data, f, indent=4)
-    """
-    {
-        polygons: []
-    }
-    """
+
+def readGeoJSON(path):
+    with open(path, "r") as f:
+        return json.load(f)['features'][0]
