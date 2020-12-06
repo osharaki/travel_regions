db77586905142d962bd46cfdeaff221d21b6a96b
Omar Sharaki <omarsharaki@gmail.com>
2020-12-06 14:17:08 +0100

    :building_construction: Remove regions from list of constructor parameters

--- src/node.py
+++ src/node.py
@@ -5,15 +5,10 @@ from region import Region
 
 class Node:
     def __init__(
-        self,
-        id: str,
-        name: str,
-        latlng: Tuple[float, float],
-        country: str,
-        regions: Dict[int, Region],
+        self, id: str, name: str, latlng: Tuple[float, float], country: str,
     ):
         self.id = id
         self.name = name
         self.latlng = (latlng,)
         self.country = (country,)
-        self.regions = regions
+        self.regions: Dict[int, Region] = {}
