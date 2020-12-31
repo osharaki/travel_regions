433fbf9f9cf9809bdd93070aaf7f14dc537af367
Omar Sharaki <omarsharaki@gmail.com>
2020-12-31 16:32:14 +0100

    :truck: Move modules from /src to /travel_regions :see_no_evil: Update path in .gitignore :truck: Add __init__.py to (sub)packages

---
+++ travel_regions/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Travel Regions."""
+
+__author__ = """Omar Sharaki"""
+__email__ = "omarsharaki@gmail.com"
+__version__ = "0.3"
---
+++ travel_regions/utils/__init__.py
@@ -0,0 +1,3 @@
+__author__ = """Omar Sharaki"""
+__email__ = "omarsharaki@gmail.com"
+__version__ = "0.3"
